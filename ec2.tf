b1f9fb24026a300f7dc363557f498308f7c3ce90
padhu86 <37106046+padhu86@users.noreply.github.com>
2025-12-14 16:26:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,7 @@
-
+resource "aws_network_interface" "trbk-scs01" {
+  subnet_id       = "subnet-05066a76a30c48c2a"
+  security_groups = ["sg-078f9b5fd75d10ee5"]
+  tags = {
+    Name = "trbk-npd-scs-01"
+  }
+}
